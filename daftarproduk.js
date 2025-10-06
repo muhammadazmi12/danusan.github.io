8c0c4d52e6db700f7f3c70783e40d8bceeec74f5
muhammadazmi12 <jemyyyy12@gmail.com>
2025-10-06 10:55:05 +0700

    Add files via upload

---
+++ daftarproduk.js
@@ -0,0 +1,7 @@
+    const products = [
+      {id:1,name:'Mie Ayam',price:5000},
+      {id:2,name:'Mochi',price:5000},
+      {id:3,name:'Telor Gulung',price:10000},
+      {id:4,name:'Cimol',price:15000},
+      {id:4,name:'Cimol',price:15000}
+    ];
